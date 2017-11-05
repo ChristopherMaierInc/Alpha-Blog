6886ad0b07c1dd6c290661c0f4e96a531e21d4ec
ChristopherMaierInc <mail@christophermaier.com.au>
2017-11-05 13:24:11 +1000

    Validations: Username/Email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_many :comments
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX=
+  /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/
+  validates :email, presence: true, length: { maximum: 100 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
 end
