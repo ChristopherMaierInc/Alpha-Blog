918120d0f7abe3a2677f8799d0885746c78a8bb8
ChristopherMaierInc <mail@christophermaier.com.au>
2017-11-05 15:57:02 +1000

    Add edit, update fuctionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
 
 root "pages#home"
 
-get "/about", to: "pages#about"
+get "about", to: "pages#about"
 
+get 'signup', to: 'users#new'
+
+resources :users, except: [:new]
 resources :articles
 
 end
