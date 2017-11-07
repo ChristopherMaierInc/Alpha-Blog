23860be4e00088df5dfdf1f420871d010d215e3b
ChristopherMaierInc <mail@christophermaier.com.au>
2017-11-07 15:12:35 +1000

    Added admin account and user/articles delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171105042204) do
+ActiveRecord::Schema.define(version: 20171107043648) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20171105042204) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
